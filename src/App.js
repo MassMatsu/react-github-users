f4237cc35b50f4eb26d4b504bb5c1d7906f70dcd
MassMatsu <masa10matsukawa@gmail.com>
2021-03-04 09:38:46 +0900

    setup router on App.js. pages - Dashboard, Login, Error (all exported from index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { GithubContext } from './context/context';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Dashboard, Error, Login } from './pages/index';
 
 const App = () => {
   const { user, requests } = React.useContext(GithubContext);
   console.log(user);
   console.log(requests);
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Dashboard />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route>
+          <Error path='*' />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
