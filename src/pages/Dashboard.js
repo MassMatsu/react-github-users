18d54ab5bf1d2add7df0f14467a6fa8ea4e99081
MassMatsu <masa10matsukawa@gmail.com>
2021-03-04 10:31:39 +0900

    setup component files. each exported to components/index.js which exports all components to Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { GithubContext } from '../context/context';
+import { Info, Repos, User, Search, Navbar } from '../components/index';
 
 const Dashboard = () => {
-  return <div>dashboard</div>;
+  const { user, requests } = React.useContext(GithubContext);
+  console.log(user);
+  console.log(requests);
+  return (
+    <main>
+      <Navbar />
+      <Search />
+      <Info />
+      <User />
+      <Repos />
+    </main>
+  );
 };
 
 export default Dashboard;
