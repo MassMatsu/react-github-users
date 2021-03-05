7652de6eda9a4f24e2b04da9781cbdfcd6676fee
MassMatsu <masa10matsukawa@gmail.com>
2021-03-05 11:22:31 +0900

    configuring repos data for charts. Pie3D and Doughnut2d chart is completed

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,10 +1,78 @@
 import React from 'react';
 import { GithubContext } from '../context/context';
+import styled from 'styled-components';
+import { Pie3D, Doughnut2D } from './Charts/index';
 
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  console.log(repos);
-  return <div>Repos</div>;
+
+  const languages = repos.reduce((total, repo) => {
+    // languages -> {JavaScript:{...}, CSS:{...}, HTML:{...}}
+
+    const { language, stargazers_count } = repo;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
+    }
+    return total;
+  }, {});
+
+  const usedLanguages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  const popularLanguages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  console.log(popularLanguages);
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Pie3D data={usedLanguages} />
+        <div></div>
+        <Doughnut2D data={popularLanguages} />
+      </Wrapper>
+    </section>
+  );
 };
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`;
 
 export default Repos;
